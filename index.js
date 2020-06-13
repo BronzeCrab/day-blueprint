0c9378f28a403a7ee205d01cdef8df723c4d4476
afoobar <gr4k@bk.ru>
2020-06-13 12:26:21 +0300

    added smooth-dnd

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import electron from 'electron';
+const electron = require('electron');
 
-cons { app } = electron;
+const { app, BrowserWindow } = electron;
+const Store = require('./store.js');
+
+// First instantiate the class
+const store = new Store({
+  configName: 'app-data'
+});
+
+const boards = store.get('boards');
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+    	webPreferences: {nodeIntegration: true}
+    });
+    mainWindow.webContents.openDevTools()
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
---
+++ store.js
@@ -0,0 +1,40 @@
+const electron = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+class Store {
+  constructor(opts) {
+    const userDataPath = (electron.app || electron.remote.app).getPath('userData');
+    this.path = path.join(userDataPath, opts.configName + '.json');
+    if (!fs.existsSync(this.path)) {
+      const initial_data = {
+        "boards": [
+          {"name": 1}, 
+          {"name": 2}
+        ]
+      }
+      fs.writeFileSync(this.path, JSON.stringify(initial_data));
+    }
+    this.data = this.parseDataFile(this.path);
+  }
+  
+  get(key) {
+    return this.data[key];
+  }
+  
+  set(key, val) {
+    this.data[key] = val;
+    fs.writeFileSync(this.path, JSON.stringify(this.data));
+  }
+
+  parseDataFile(filePath) {
+    try {
+    return JSON.parse(fs.readFileSync(filePath));
+    } catch(error) {
+    return;
+    }   
+  }   
+
+}
+
+module.exports = Store;
